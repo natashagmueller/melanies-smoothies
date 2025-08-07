805454c0b33608a8bbe5e5645bc3eb01b66779df
Natasha Gomes Mueller <51997290+natashagmueller@users.noreply.github.com>
2025-08-07 15:34:43 +1000

    Update streamlit_app.py
    format of the response to json

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-# New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-
-
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -28,6 +22,12 @@ ingredients_list = st.multiselect(
     my_dataframe,
     max_selections=5
 )
+
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
+
 if ingredients_list:
     ingredients_string = ''
     
